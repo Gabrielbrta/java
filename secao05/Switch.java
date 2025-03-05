aff08eeccf09e591f50e698c39f6a77418025a47
Gabrielbrta <vdvsa159946@gmail.com>
2025-03-05 17:52:36 -0300

    switch

---
+++ secao05/Switch.java
@@ -0,0 +1,23 @@
+package secao05;
+
+public class Switch {
+
+    public static void main(String[] args){
+        int idade = 0;
+        
+        switch(idade) {
+            case 17:
+                System.out.println("é menor de idade");
+                break;
+            case 18:
+                System.out.println("já é adulto 18");
+                break;
+            case 19:
+                System.out.println("tem 19 anos");
+                break;
+            default:
+                System.out.println("Não existe");
+                break;
+        }
+    }
+}
